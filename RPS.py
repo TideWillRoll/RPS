98cad54082743d132cf5269b4091b611f0ba5028
Will Childress <139587629+TideWillRoll@users.noreply.github.com>
2023-07-26 14:05:30 -0400

    Updated RPS
    I've added some more to the original but I can't get the game to announce the result on the repeated occurrence.

--- RPS.py
+++ RPS.py
@@ -1,19 +1,22 @@
 import random
-name=input("Enter your name : ")
 def play():
     options=['Rock', 'Paper', 'Scissors']
     user = input("What's your choice? 'r' for rock, 'p' for paper, 's' for scissors\n")
     computer = random.choice(['r', 'p', 's'])
     if user == computer:
         return 'It\'s a tie!'
-    # r > s, s > p, p > r
     if is_win(user, computer):
-        return 'You won!'
+        return 'You won'
     else:
         return("You lost! Better luck next time!")
 def is_win(player, opponent):
-    # return true if player wins
-    # r > s, s > p, p > r
     if (player == 'r' and opponent == 's') or (player == 's' and opponent == 'p') or (player == 'p' and opponent == 'r'):
         return True
+def script():
+    restart = input("Would you like play again?")
+    if restart == "yes" or restart == "y":
+        play()
+    if restart == "no" or restart == "n":
+        print("Thanks for playing!")
 print(play())
+script()
