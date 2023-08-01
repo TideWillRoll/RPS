fe6f1f07526b4621140974c57c3bb10fe37c2de8
TideWillRoll <wtc19@comcast.net>
2023-08-01 13:55:25 -0400

    Update RPS.py

--- RPS.py
+++ RPS.py
@@ -1,19 +1,28 @@
 import random
-name=input("Enter your name : ")
 def play():
     options=['Rock', 'Paper', 'Scissors']
     user = input("What's your choice? 'r' for rock, 'p' for paper, 's' for scissors\n")
     computer = random.choice(['r', 'p', 's'])
     if user == computer:
-        return 'It\'s a tie!'
-    # r > s, s > p, p > r
-    if is_win(user, computer):
-        return 'You won!'
+        return 'You tied!'
+    if is_win(user, computer) == True:
+        return 'Congrats! You Won!'
     else:
-        return("You lost! Better luck next time!")
+        return "You lost! Better luck next time!"
 def is_win(player, opponent):
-    # return true if player wins
-    # r > s, s > p, p > r
     if (player == 'r' and opponent == 's') or (player == 's' and opponent == 'p') or (player == 'p' and opponent == 'r'):
         return True
+def script():
+    restart = input("Play again?")
+    if restart == "yes" or restart == "y" or restart == True:
+        print(play())
+        print(script())
+    if restart == "no" or restart == "n" or restart == False:
+        print("Thanks for playing!")
+def score():
+    score = 0
+    while play():
+        if is_win == True:
+            print(score(+1)) 
 print(play())
+print(script())
