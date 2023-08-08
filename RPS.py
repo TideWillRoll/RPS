2f349e81f69d914d3276d3b4f47fd514a038ea49
TideWillRoll <wtc19@comcast.net>
2023-08-08 14:01:04 -0400

    Update RPS.py

--- RPS.py
+++ RPS.py
@@ -1,23 +1,26 @@
 import random
 def play():
-    options=['Rock', 'Paper', 'Scissors']
-    user = input("What's your choice? 'r' for rock, 'p' for paper, 's' for scissors\n")
-    computer = random.choice(['r', 'p', 's'])
-    if user == computer:
-        return 'You tied!'
-    if is_win(user, computer) == True:
-        return 'Congrats! You Won!'
-    else:
-        return "You lost! Better luck next time!"
-def is_win(player, opponent):
-    if (player == 'r' and opponent == 's') or (player == 's' and opponent == 'p') or (player == 'p' and opponent == 'r'):
-        return True
+    choices=('Rock', 'Paper', 'Scissors')
+    user_choice = input(choices)
+    computer_choice = random.choice(choices)
+    is_win = (user_choice == 'Rock' and computer_choice == 'Scissors') or (user_choice == 'Paper' and computer_choice == 'Rock') or (user_choice == 'Scissors' and computer_choice == 'Paper')
+    is_loss = (computer_choice == 'Paper' and user_choice == 'Rock') or (computer_choice == 'Rock' and user_choice == 'Scissors') or (computer_choice == 'Scissors' and user_choice == 'Rock')
+    tie = (user_choice == 'Rock' and computer_choice == 'Rock') or (user_choice == 'Paper' and computer_choice == 'Paper') or (user_choice == 'Scissors' and computer_choice == 'Scissors') 
+    print(f"You chose {user_choice}, Computer chose {computer_choice}")
+    if is_win:
+        print("You won!") 
+    elif is_loss:
+        print("You lost! Better luck next time!")
+    elif tie:
+        print("That's a tie!")    
 def script():
     restart = input("Play again?")
-    if restart == "yes" or restart == "y" or restart == True:
-        print(play())
-        print(script())
-    if restart == "no" or restart == "n" or restart == False:
-        print("Thanks for playing!")
-print(play())
-print(script())
+    while True:
+        if restart == "Yes" or restart == "yes":
+            play()
+            script()
+            continue
+        else:
+            print("Thanks for playing!")        
+play()
+script()
